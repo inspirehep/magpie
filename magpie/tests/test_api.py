f6eaa852bac3239de3143e2bf0aca2a62f4871b8
Jan Stypka <jan.stypka@cern.ch>
2016-01-12 13:45:22 +0100

    tests: add verbose=False

--- magpie/tests/test_api.py
+++ magpie/tests/test_api.py
@@ -8,17 +8,17 @@ from magpie.config import HEP_TEST_PATH, MODEL_PATH
 def train_model_if_necessary(model_path):
     """ If the model under a given path does not exist, create one. """
     if not os.path.exists(model_path):
-        api.train(model_path=model_path)
+        api.train(model_path=model_path, verbose=False)
 
 
 class TestAPI(unittest.TestCase):
     """ Basic tests for making sure that the API works """
     def test_train(self):
-        api.train()
+        api.train(verbose=False)
 
     def test_test(self):
         train_model_if_necessary(MODEL_PATH)
-        precision, recall, f1_score, accuracy = api.test()
+        precision, recall, f1_score, accuracy = api.test(verbose=False)
         self.assertGreater(precision, 0)
         self.assertGreater(recall, 0)
         self.assertGreater(f1_score, 0)
@@ -26,9 +26,9 @@ class TestAPI(unittest.TestCase):
 
     def test_extract(self):
         train_model_if_necessary(MODEL_PATH)
-        candidates = api.extract(HEP_TEST_PATH + '/524510.txt')
+        candidates = api.extract(HEP_TEST_PATH + '/524510.txt', verbose=False)
         self.assertGreaterEqual(len(candidates), 0)
 
     def test_candidate_recall(self):
-        recall = api.calculate_recall_for_kw_candidates()
+        recall = api.calculate_recall_for_kw_candidates(verbose=False)
         self.assertGreater(recall, 0)
