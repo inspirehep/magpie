058f55b3a1705fe17384da640f1f288f1117c60e
Jan Stypka <stypka@spotify.com>
2017-10-08 14:24:25 +0200

    tests: add a separate test for RNN and batch training

--- magpie/tests/test_api.py
+++ magpie/tests/test_api.py
@@ -8,7 +8,7 @@ DATA_DIR = os.path.join(PROJECT_DIR, 'data', 'hep-categories')
 
 class TestAPI(unittest.TestCase):
 	""" Basic integration test """
-	def test_integrity(self):
+	def test_cnn_train(self):
 		# Get them labels!
 		with io.open(DATA_DIR + '.labels', 'r') as f:
 			labels = {line.rstrip('\n') for line in f}
@@ -17,7 +17,28 @@ class TestAPI(unittest.TestCase):
 		from magpie import MagpieModel
 		model = MagpieModel()
 		model.init_word_vectors(DATA_DIR, vec_dim=100)
-		history = model.train(DATA_DIR, labels, test_ratio=0.3, epochs=3)
+		history = model.train(DATA_DIR, labels, nn_model='cnn', test_ratio=0.3, epochs=3)
+		assert history is not None
+
+		# Do a simple prediction
+		predictions = model.predict_from_text("Black holes are cool!")
+		assert len(predictions) == len(labels)
+
+		# Assert the hell out of it!
+		for lab, val in predictions:
+			assert lab in labels
+			assert 0 <= val <= 1
+
+	def test_rnn_batch_train(self):
+		# Get them labels!
+		with io.open(DATA_DIR + '.labels', 'r') as f:
+			labels = {line.rstrip('\n') for line in f}
+
+		# Run the model
+		from magpie import MagpieModel
+		model = MagpieModel()
+		model.init_word_vectors(DATA_DIR, vec_dim=100)
+		history = model.batch_train(DATA_DIR, labels, nn_model='rnn', epochs=3)
 		assert history is not None
 
 		# Do a simple prediction
